8b793c1d95928ea42f388e1f2f86c5e95f26402e
Jie Yang <yangzuojie@gmail.com>
2012-04-15 17:22:07 +0800

    Begin to implement tags manager page

---
+++ js/tags.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('#tags-vocabulary-select').change(function() {
+    if($(this).val() == 0) {
+      $('#tags-term-div').html('');
+    } else {
+      $.get(Drupal.settings.basePath + 'tags/vocabulary/' + $(this).val() + '/ajax', {}, function(html) {
+        $('#tags-term-div').html(html);
+      });
+    }
+  });
+});
--- template/dashboard-page.tpl.php
+++ template/dashboard-page.tpl.php
@@ -1,58 +1,13 @@
 <?php
 ?>
-<script type="text/javascript">
-  $(function(){
-    $('#taxonomy-tt').tree({
-      data: [<?php echo json_encode($data['data_list']) ?>],
-      dnd: true,
-      animate: true,
-      onClick:function(node){
-        $(this).tree('beginEdit', node.target);
-      },
-      onAfterEdit:function(node){
-        var parent = $('#taxonomy-tt').tree('getParent', node.target).id;
-        $.post( '<?php echo url('tags/save') ?>', {id: node.id, parent: parent, text: node.text}, function(data){
-            node.id = data.id;
-            $('#taxonomy-tt').tree('update', node);
-        }, 'json');
-      },
-      onContextMenu: function(e, node){
-        e.preventDefault();
-        $('#taxonomy-tt').tree('select', node.target);
-        $('#mm').menu('show', {
-          left: e.pageX,
-          top: e.pageY
-        });
-      },
-      onDrop: function(targetNode, source, point){
-        var targetId = $('#taxonomy-tt').tree('getNode', targetNode).id;
-        $.post( '<?php echo url('tags/drop') ?>', {id: source.id, target_id: targetId, point: point}, function(data){
-        }, 'json');
-      }
-    });
-  });
-  function append(){
-    var node = $('#taxonomy-tt').tree('getSelected');
-    $('#taxonomy-tt').tree('append',{
-      parent: (node?node.target:null),
-      data:[{
-        text:'new1',
-        checked:true
-      }]
-    });
-  }
-  function remove(){
-    var node = $('#taxonomy-tt').tree('getSelected');
-    var parent = $('#taxonomy-tt').tree('getParent', node.target).id;
-    $.post( '<?php echo url('tags/remove') ?>', {id: node.id, parent: parent}, function(data){
-      $('#taxonomy-tt').tree('remove', node.target);
-    }, 'json');
-  }
-</script>
+<div id="tags-vocabulary-div">
+<select id="tags-vocabulary-select">
+<option value="0"><?php echo t('- Please choose -') ?></option>
+<?php foreach($data['vocabulary_list'] as $vocabulary): ?>
+<option value="<?php echo $vocabulary->vid ?>"><?php echo $vocabulary->name ?></option>
+<?php endforeach; ?>
+</select>
+</div>
 
-<ul id="taxonomy-tt"></ul>
-
-<div id="mm" class="easyui-menu" style="width:120px;">
-  <div onclick="append()" iconCls="icon-add">Append</div>
-  <div onclick="remove()" iconCls="icon-remove">Remove</div>
+<div id="tags-term-div">
 </div>
