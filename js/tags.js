58306494bdfd118e01a41993731bdb2dbda5695a
Jie Yang <yangzuojie@gmail.com>
2012-04-18 01:07:44 +0800

    Temporary version

--- js/tags.js
+++ js/tags.js
@@ -3,9 +3,14 @@ $(function() {
     if($(this).val() == 0) {
       $('#tags-term-div').html('');
     } else {
-      $.get(Drupal.settings.basePath + 'tags/manage/vocabulary/' + $(this).val() + '/ajax', {}, function(html) {
-        $('#tags-term-div').html(html);
-      });
+      $.get(Drupal.settings.basePath + 'tags/manage/vocabulary/' + $(this).val() + '/ajax', {}, function(data) {
+        if(data.hide) {
+          $('#add-term-submit').hide();
+        } else {
+          $('#add-term-submit').show();
+        }
+        $('#tags-term-div').html(data.html);
+      }, 'json');
     }
   });
 });
