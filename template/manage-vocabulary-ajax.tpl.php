1bc752dbf34317c73f4d8011388fba682ba96307
Jie Yang <yangzuojie@gmail.com>
2012-04-17 23:04:37 +0800

    Re factory

--- template/manage-vocabulary-ajax.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-?>
-<select multiple="true" id="tags-term-select">
-<?php foreach($data['term_list'] as $term): ?>
-<option><?php echo $term->name ?></option>
-<?php endforeach; ?>
-</select>
