f195c724ff0e1ca436be2fe7f6822c21d4584de9
Jie Yang <yangzuojie@gmail.com>
2012-04-18 21:48:29 +0800

    Add title vocabularies

--- js/tags.js
+++ js/tags.js
@@ -2,6 +2,7 @@ $(function() {
   $('#tags-vocabulary-select').change(function() {
     if($(this).val() == 0) {
       $('#tags-term-div').html('');
+      $('#add-term-submit').hide();
     } else {
       $.get(Drupal.settings.basePath + 'tags/manage/vocabulary/' + $(this).val() + '/ajax', {}, function(data) {
         if(data.hide) {
