384574da25e140da4bc61ee3edf2a6d253460b26
Jie Yang <yangzuojie@gmail.com>
2012-04-19 14:40:05 +0800

    Implement show node content by AJAX

--- template/content-page.tpl.php
+++ template/content-page.tpl.php
@@ -3,21 +3,34 @@ $(function(){
   $('#tags-tt').tree({
     url: '<?php echo url('tags/content/ajax/tree') ?>',
     onClick:function(node){
-      
     }
   });
+
+  $('#tags-content-table').datagrid({
+    url: '<?php echo url('tags/content/ajax/node') ?>',
+    border: false,
+    fit: true,
+    fitColumns: true,
+    onClickRow: function(rowIndex, rowData) {
+      $.get('<?php echo url('tags/content/ajax/node') ?>' + '/' + rowData.nid, function(html) {
+        $('#tags-node-content').html(html);
+      })
+    },
+  });
+
 });
 </script>
 
 <div class="easyui-layout" style="width:100%;height:480px;">
   <div region="east" title="&nbsp;" split="true" style="width:480px;">
+    <div id="tags-node-content"></div>
   </div>
   <div region="west" split="true" title="&nbsp;" style="width:180px;">
     <ul id="tags-tt"></ul>
   </div>
 
   <div region="center" title="&nbsp;" style="background:#fafafa;overflow:hidden">
-    <table class="easyui-datagrid" url="<?php echo url('tags/content/ajax/node') ?>" border="false" fit="true" fitColumns="true">
+    <table id="tags-content-table">
       <thead>
         <tr>
           <th field="no" width="5"><?php echo t('NO.') ?></th>
